a965a15abde5e0b3c4e91f0b415052f6501920b4
mbugua.26168@students.kyu.ac.ke <mbugua.26168@students.kyu.ac.ke>
2025-10-11 18:11:02 +0300

    Add files via upload

---
+++ Decrement (1).c
@@ -0,0 +1,18 @@
+//simple C program
+/*
+Name: Mbugua Ankal
+Reg no:CT100/G/26168/25
+Description: to display 100 to 50
+*/
+
+#include <stdio.h>//
+
+//main function
+int main() {
+    int i;
+for(i=100; i>=50; i--){
+printf("%d \t", i);
+}
+
+    return 0;
+}
